65c29e25fc2d5a8d934d5264bd5290e37008b67c
Clif <cliftonf87@gmail.com>
2018-04-06 16:26:31 -0700

    Ported over prototype code to the new Unity project; Added timer, added current level text, and Added game object skeleton for win/lose text

---
+++ game/Assets/Scripts/Level.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Level : MonoBehaviour {
+
+	public Text levelText;
+
+	// Use this for initialization
+	void Start () {
+		// level is set to the scene name
+		levelText.text = Application.loadedLevelName.ToString();
+	}
+}
---
+++ game/Assets/Scripts/Timer.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Timer : MonoBehaviour {
+
+	public Text timerText;
+	private float startTime;
+
+	// Use this for initialization
+	void Start () {
+		// timer will start as soon as we enter game mode
+		startTime = Time.time;
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float time = Time.time - startTime;
+		string minutes = ((int) time / 60).ToString();
+		string seconds = (time % 60).ToString("00");
+
+		timerText.text = "Time " + string.Format("{0:00}:{1:00}", minutes, seconds);
+	}
+}
