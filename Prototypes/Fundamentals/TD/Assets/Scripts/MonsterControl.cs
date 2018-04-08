3d53370c5a50ca2b774075ede7f8d40b5f6630c7
Phani Teja Kantamneni <teja.kantamneni@outlook.com>
2018-04-08 01:58:26 -0700

    Added enemy movement prototype script

---
+++ Prototypes/Fundamentals/TD/Assets/Scripts/MonsterControl.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MonsterControl : MonoBehaviour
+{
+
+    [SerializeField]
+    Transform[] waypoints;
+
+    [SerializeField]
+    float moveSpeed = 2f;
+
+    public int waypointIndex = 0;
+
+    void Start()
+    {
+        transform.position = waypoints[waypointIndex].transform.position;
+    }
+
+    void Update()
+    {
+        Move();
+    }
+
+    void Move()
+    {
+        transform.position = Vector2.MoveTowards(transform.position,
+                                                waypoints[waypointIndex].transform.position,
+                                                moveSpeed * Time.deltaTime);
+
+        if (transform.position == waypoints[waypointIndex].transform.position)
+        {
+            waypointIndex += 1;
+        }
+
+    }
+
+}
---
+++ Prototypes/Fundamentals/TD/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerController : MonoBehaviour {
+
+    private Rigidbody2D rb2d;
+    public float speed;
+    private int count;
+    public Text textCount;
+    public Text WinText;
+    void Start()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+        count = 0;
+        WinText.text = "";
+        SetCountText();
+    }
+    private void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+        Vector2 movement = new Vector2(moveHorizontal, moveVertical);
+        rb2d.AddForce(movement * speed);
+    }
+    
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("PickUp"))
+        {
+            other.gameObject.SetActive(false);
+            count += 1;
+            SetCountText();
+        }
+    }
+
+    void SetCountText()
+    {
+        textCount.text = "Count:" + count.ToString();
+        if (count >= 3)
+            WinText.text = "You Win";
+    }
+}
