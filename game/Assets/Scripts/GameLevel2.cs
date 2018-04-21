fb7364880ea3ee168f2d8529b23ae451a01ee94a
Animesh Grover <animesh70@gmail.com>
2018-04-21 16:10:00 -0700

    updated level2 and minor changes

---
+++ game/Assets/Scripts/GameLevel2.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class GameLevel2 : GameSystem, IGameSystem
+{
+
+    // change game level here
+    private static int LEVEL = 2;
+
+    private bool isPaused;
+    private Timer timer;
+    private MonsterFactory mf;
+
+    void Start()
+    {
+        Run();
+    }
+
+    void Update()
+    {
+        if (Input.GetButtonDown("Cancel"))
+        {
+            isPaused = this.ToggleGamePause();
+        }
+    }
+
+    public override void Initialize()
+    {
+        isPaused = false;
+        MovePattern.setInstance(LEVEL);
+
+        mf = new MonsterFactory(new Vector3(-11.5f, 0.4f, 1));  // starting coordinate for enemies
+        monsters = new ArrayList();
+        StartCoroutine(Spawn());
+
+        timer = GameObject.Find("Canvas/Timer Text").GetComponent<Timer>();
+    }
+
+    public override void Play()
+    {
+        timer.Run();
+    }
+
+    public override void End()
+    {
+
+    }
+
+    // how the enemies will be spawned, use enum "Monsters" to select the enemy you want to spawn
+    public IEnumerator Spawn()
+    {
+        monsters.Add(mf.getMonster(Monsters.bt));
+        yield return new WaitForSeconds(2);
+        monsters.Add(mf.getMonster(Monsters.bt));
+        yield return new WaitForSeconds(2);
+        monsters.Add(mf.getMonster(Monsters.bt));
+        yield return new WaitForSeconds(2);
+    }
+
+    public int Level
+    {
+        get
+        {
+            return LEVEL;
+        }
+    }
+
+    public bool IsPaused
+    {
+        get
+        {
+            return isPaused;
+        }
+    }
+
+}
